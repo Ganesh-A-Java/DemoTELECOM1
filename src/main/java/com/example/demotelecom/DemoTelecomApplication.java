2758571722066d288ad3a88813ee4b6e7b3a3eeb
abhi <abhilashvkhot>
2022-10-14 10:57:19 +0530

    First commit

---
+++ src/main/java/com/example/demotelecom/DemoTelecomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTelecomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTelecomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demotelecom/DemoTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotelecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTelecomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
